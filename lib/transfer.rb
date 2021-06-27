9d3ecee6f37d9f94d656befe486e43ee30a4a153
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2021-06-27 20:47:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,7 +29,7 @@ class Transfer
   end
   
   def reverse_transfer
-    if @status == "complete"
+    if @status == "complete" #it can only reverse executed transfers
       @sender.balance += @amount
       @receiver.balance -= @amount
       @status = "reversed"
